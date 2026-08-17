1602c9ac11a08a24778e7bf793a62eccd03c3f38
Juan Manuel Landa <jmlanda93@gmail.com>
2026-08-17 20:11:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def init_chain():
     try:
         from langchain_groq import ChatGroq
         api_key = st.secrets["GROQ_API_KEY"]
-        llm = ChatGroq(api_key=api_key, model_name="gpt-oss-20b", temperature=0)
+        llm = ChatGroq(api_key=api_key, model_name="openai/gpt-oss-20b", temperature=0)
     
     # Si da error (estamos en local/Docker sin el archivo secrets), usar Ollama
     except Exception:
